d21ad27144141f45ebb3f401c010b4c22dfdea55
Alejandro Tarazona <luisalejo0602@gmail.com>
2019-10-31 13:20:20 -0500

    create method to return the student's career

---
+++ server/scripts/users-career.js
@@ -0,0 +1,32 @@
+/**
+ * Returns user's career
+ * @param {String} userId user's _id
+ * @param {Object} studentModel Student loopback model
+ * @param {Function} callback loopback callback
+ */
+const findUsersCareer = async (userId, studentModel, callback) => {
+  const user = await studentModel.findOne({ where: { _id: userId } });
+  if (!user) {
+    const error = Error('No se encontró al usuario');
+    error.name = 'Not Found';
+    error.stack = undefined;
+    error.statusCode = 404;
+    callback(error);
+  }
+  const userCareer = user.career;
+  const career = {
+    name: userCareer.name,
+    currentAverage: userCareer.currentAverage,
+    lines: userCareer.lines.map(line => ({
+      name: line.name,
+      color: line.color,
+      courses: line.courses.map(course => ({
+        name: course.name,
+        semester: course.semester
+      }))
+    }))
+  };
+  callback(null, career);
+};
+
+module.exports = { findUsersCareer };
